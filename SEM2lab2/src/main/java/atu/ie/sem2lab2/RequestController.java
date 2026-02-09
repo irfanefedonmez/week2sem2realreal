5b80e591e7fc8b4128edfb756eddd813cff210b8
IrfanEfeDonmez-STUDE <G00482282@atu.ie>
2026-02-09 12:03:24 +0000

    finished with viedo lab and gonna start the example

--- SEM2lab2/src/main/java/atu/ie/sem2lab2/RequestController.java
+++ SEM2lab2/src/main/java/atu/ie/sem2lab2/RequestController.java
@@ -1,4 +1,28 @@
 package atu.ie.sem2lab2;
 
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping
 public class RequestController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World";
+
+    }
+    @GetMapping("/greet/{name}")
+    public String greet(@PathVariable String name) {
+        return "Hello " + name;
+    }
+
+    @GetMapping("/details")
+    public String details(@RequestParam String name, @RequestParam int age) {
+        return "Hello " + name + " Age " + age;
+    }
+
+    @GetMapping("/person")
+    public Person getPerson() {
+        return new Person("paul",25);
+    }
 }
